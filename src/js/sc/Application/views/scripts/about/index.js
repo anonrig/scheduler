0a189fdd90ecf2627260c31c6c2328f6e5e4784d
dashersw <armagan@amcalar.com>
2014-01-23 04:38:54 +0200

    Fix about view css.

--- src/js/sc/Application/views/scripts/about/index.js
+++ src/js/sc/Application/views/scripts/about/index.js
@@ -7,13 +7,13 @@ goog.provide('sc.views.scripts.about.index');
  */
 sc.views.scripts.about.index = function() {
     return '<div class="container">' +
-        '<div class="detail view active">' +
+        '<div class="about view active">' +
         '<h1>The Team</h1>' +
         '<h2>Armagan Amcalar</h2>' +
         '<h2>Yagiz Nizipli</h2>' +
         '<h2>Mert Tacir</h2>' +
         '<h1>Contact Us</h1>' +
-        '<h3>Via http://schedul.re</h3>'
+        '<h3>Via http://schedul.re</h3>' +
         '</div>' +
         '</div>';
 };
