c6c32c37ef1c4353c78f5604c09d22e7b5650380
Yagiz Nizipli <yagiznizipli@me.com>
2014-08-18 13:51:29 +0300

    Fix iOS8 height bug in carousel view.

--- src/js/sc/components/Login/Template.js
+++ src/js/sc/components/Login/Template.js
@@ -23,23 +23,23 @@ goog.inherits(sc.components.Login.Template, tart.components.mobile.Template);
 sc.components.Login.Template.prototype.base = function(items) {
     return '<div class="loginCarousel">' +
             '<div class="items">' +
-                '<div class="item view"><img src="img/search.png" height="60%"/>' +
+                '<div class="item view"><img src="img/search.png"/>' +
                     '<div class="itemTitle">Search for courses</div>' +
                     '<div class="itemInfo">Sift through all available courses by their ID or other information.</div>' +
                 '</div>' +
-                '<div class="item view"><img src="img/search2.png" height="60%"/>' +
+                '<div class="item view"><img src="img/search2.png" />' +
                     '<div class="itemTitle">Select Courses To Keep In Schedule</div>' +
                     '<div class="itemInfo">Swipe left on any course to add it to your schedule. Scheduler will notify you if there\'s a collision.</div>' +
                 '</div>' +
-                '<div class="item view"><img src="img/detailpage.png" height="60%"/>' +
+                '<div class="item view"><img src="img/detailpage.png" />' +
                     '<div class="itemTitle">Detailed Course Information</div>' +
                     '<div class="itemInfo">Tap on any course to see useful information such as lecture details, summary and admission levels.</div>' +
                 '</div>' +
-                '<div class="item view"><img src="img/coursecard.png" height="60%"/>' +
+                '<div class="item view"><img src="img/coursecard.png" />' +
                 '<div class="itemTitle">A Shortcut For All of Your Courses</div>' +
                 '<div class="itemInfo">The courses you add to your schedule will be available in the Courses Card. Just tap it.</div>' +
                 '</div>' +
-                '<div class="item view"><img src="img/schedule.png" height="60%"/>' +
+                '<div class="item view"><img src="img/schedule.png" />' +
                 '<div class="itemTitle">The Whole Week</div>' +
                 '<div class="itemInfo">Swipe right on search to see your schedule at a glance, along with all your empty slots.</div>' +
                 '</div>' +
